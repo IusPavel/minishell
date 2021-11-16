a84d289b0b0986c46955feb693761367c7c55868
prochell <prochell@student.21-school.ru>
2021-11-16 20:58:47 +0300

    fix pwd

--- src/minishell_pwd.c
+++ src/minishell_pwd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell_pwd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: signacia <signacia@student.42.fr>          +#+  +:+       +#+        */
+/*   By: prochell <prochell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/05 16:28:21 by prochell          #+#    #+#             */
-/*   Updated: 2021/11/12 21:33:55 by signacia         ###   ########.fr       */
+/*   Updated: 2021/11/16 20:42:50 by prochell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ int	get_pwd(t_shell *minishell, char **str)
 	if (!ft_strcmp("pwd", str[0]))
 	{
 		tmp = getcwd(NULL, 0);
+		if (tmp == NULL)
+			return (0);
 		write(1, tmp, ft_strlen(tmp));
 		write(1, "\n", 1);
 		free(tmp);
